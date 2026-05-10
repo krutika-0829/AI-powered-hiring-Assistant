e16323cd75252f009ae7d56dfa100d3eec59a59d
krutika-0829 <krutikavairagade424@gmail.com>
2026-05-10 13:36:54 +0530

    Add files via upload

---
+++ storage.py
@@ -0,0 +1,23 @@
+import faiss
+import numpy as np
+from sentence_transformers import SentenceTransformer
+
+
+def embeddings(chunks):
+    if not chunks:
+        return None, None
+    
+
+    texts = [chunk.page_content for chunk in chunks]
+
+    model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
+
+    embeddings = model.encode(texts)
+    embeddings = np.array(embeddings).astype("float32")
+
+    dimension = embeddings.shape[1]
+
+    index = faiss.IndexFlatL2(dimension)
+    index.add(embeddings)
+
+    return index,model
