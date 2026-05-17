9689057958ebc0086b1ec7ce69f634d9c3c0764b
krutika-0829 <krutikavairagade424@gmail.com>
2026-05-17 20:22:15 +0530

    Update storage.py

--- storage.py
+++ storage.py
@@ -1,24 +1,59 @@
+import os
 import faiss
 import numpy as np
-from sentence_transformers import SentenceTransformer
+
+USE_OPENAI = os.getenv("USE_OPENAI", "false").lower() == "true"
 
 
 def embeddings(chunks):
+
     if not chunks:
         return None, None
-    
 
     texts = [chunk.page_content for chunk in chunks]
 
-    os.environ["TOKENIZERS_PARALLELISM"] = "false"
-    model = SentenceTransformer("all-MiniLM-L6-v2", device="cpu")  
+   
+    if USE_OPENAI:
 
-    vectors = model.encode(texts, batch_size=16, show_progress_bar=False)
-    embeddings = np.array(embeddings).astype("float32")
+        from openai import OpenAI
 
-    dimension = embeddings.shape[1]
+        client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+        response = client.embeddings.create(
+            model="text-embedding-3-small",
+            input=texts
+        )
+
+        embedding_vectors = [
+            item.embedding for item in response.data
+        ]
+
+        embedding_vectors = np.array(
+            embedding_vectors
+        ).astype("float32")
+
+        model = "openai"
+
+  
+    else:
+
+        from sentence_transformers import SentenceTransformer
+
+        model = SentenceTransformer(
+            "sentence-transformers/all-MiniLM-L6-v2"
+        )
+
+        embedding_vectors = model.encode(texts)
+
+        embedding_vectors = np.array(
+            embedding_vectors
+        ).astype("float32")
+
+   
+    dimension = embedding_vectors.shape[1]
 
     index = faiss.IndexFlatL2(dimension)
-    index.add(embeddings)
 
-    return index,model
+    index.add(embedding_vectors)
+
+    return index, model
